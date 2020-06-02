0fa575264bd0433ff8e58ce620a388f46a468c0c
Jay Reginaldo <allmotorjay@gmail.com>
2020-06-02 14:42:17 -0700

    Updates

---
+++ precache-manifest.c2578b6cccf3c98201c4fba115ffd7fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f57072bae2a968a18add2a13b62a8b7f",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "a4698eefc22dd26899ce",
+    "url": "/project-4-assessment/static/css/main.d5eabd75.chunk.css"
+  },
+  {
+    "revision": "3357667a71712448bd75",
+    "url": "/project-4-assessment/static/js/2.2018531e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.2018531e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4698eefc22dd26899ce",
+    "url": "/project-4-assessment/static/js/main.99d25bb0.chunk.js"
+  },
+  {
+    "revision": "11176ca57580c991af7e",
+    "url": "/project-4-assessment/static/js/runtime-main.548f4f76.js"
+  }
+]);
